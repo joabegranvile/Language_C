4bbb50d53c1f87b8ae73bb7d408c939315c30b05
Joabe <joabe.granvile@yahoo.com>
2024-12-01 18:23:29 -0400

    traversingArr

---
+++ pointers/traversingArraybyPointer.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+
+int main() {
+	int a[] = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
+	int len = sizeof(a)/sizeof(int);
+	int *x = a;
+	int i = 0;
+
+
+	for (i = 0; i < len; i++) {
+		printf("Address of subscript %d = %d Value = %d\n", i, x, *x);
+		x++;
+	}
+}
